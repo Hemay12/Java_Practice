e435e86443d9f83772d7b288089806f0bba08da0
Hemay12 <hemaysorathia1@gmail.com>
2024-12-28 18:00:28 +0530

    Stop the iteration where element is greater than next element

--- src/main/java/top20mostAsked/elementStartDecrease.java
+++ src/main/java/top20mostAsked/elementStartDecrease.java
@@ -1,4 +1,16 @@
 package top20mostAsked;
 
 public class elementStartDecrease {
+    public static void main (String [] args){
+
+        int [] arr = {1,2,3,2,1};
+        int a =0;
+        for(int i =0; i<arr.length -1; i++){
+            if(arr[i] > arr[i+1]){
+                a =arr[i];
+                break;
+            }
+        }
+        System.out.println(a);
+    }
 }
